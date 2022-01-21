5cdedc0e608ba116a8d330f3cadc18ab82409fe5
Ben Thomasson <bthomass@redhat.com>
2022-01-21 14:50:46 -0500

    pprint

--- receiver.py
+++ receiver.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python
 import os
-from azure.servicebus import ServiceBusClient, ServiceBusMessage
+import json
+from azure.servicebus import ServiceBusClient
+from pprint import pprint
 
 CONNECTION_STR = os.environ['CONNECTION_STR']
 QUEUE_NAME = os.environ['QUEUE_NAME']
 
 
-servicebus_client = ServiceBusClient.from_connection_string(conn_str=CONNECTION_STR, logging_enable=True)
+servicebus_client = ServiceBusClient.from_connection_string(conn_str=CONNECTION_STR,
+                                                            logging_enable=True)
 
 with servicebus_client:
     receiver = servicebus_client.get_queue_receiver(queue_name=QUEUE_NAME, max_wait_time=600)
     with receiver:
         for msg in receiver:
-            print("Received: " + str(msg))
+            data = json.loads(str(msg))
+            print('='*80)
+            pprint(data)
             receiver.complete_message(msg)
