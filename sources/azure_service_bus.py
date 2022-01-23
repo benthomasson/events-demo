efff012c770bb5c7fe78a6f04cda5d6925b686d5
Ben Thomasson <bthomass@redhat.com>
2022-01-23 11:46:51 -0500

    Adds modules, sources, and transforms

---
+++ sources/azure_service_bus.py
@@ -0,0 +1,14 @@
+
+
+def main(queue, args):
+
+    servicebus_client = ServiceBusClient.from_connection_string(conn_str=args[conn_str],
+                                                                logging_enable=args.get(logging_enable, True))
+
+    with servicebus_client:
+        receiver = servicebus_client.get_queue_receiver(queue_name=args[queue_name], max_wait_time=600)
+        with receiver:
+            for msg in receiver:
+                data = json.loads(str(msg))
+                queue.put(data)
+                receiver.complete_message(msg)
